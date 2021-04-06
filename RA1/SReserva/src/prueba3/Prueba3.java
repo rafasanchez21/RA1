c0907a731eeaa220bea29246520ebd12190391be
rafasanchez21 <72165285+rafasanchez21@users.noreply.github.com>
2021-04-06 17:19:19 +0200

    Add files via upload

---
+++ RA1/SReserva/src/prueba3/Prueba3.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 34620
+ */
+public class Prueba3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+
+     
+        
+    InputStreamReader entrada = new InputStreamReader(System.in);
+    BufferedReader teclado = new BufferedReader (entrada);
+   
+
+    //Líneas de código leidas por SReserva
+    System.out.println("Que juego desea alquilar? (Deportes, Aventuras o Terror)");
+    System.out.println("FIFA || NBA  || F1 ");
+    System.out.println("GTA V || ARK || Minecraft");
+    System.out.println("Outlast || Until Dawn || Amnesia");
+    System.out.println("Cuantos dias desea reservarlo? (1-30)");
+        
+   
+
+    }
+    }
+
